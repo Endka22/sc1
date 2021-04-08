6ab074f196038bbcab46d82aaf5b6ebb69611c1d
bokiroke1 <79509933+bokiroke1@users.noreply.github.com>
2021-04-08 14:02:02 +0700

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -277,11 +277,11 @@ mkdir -p /run/pluto
 service fail2ban restart 2>/dev/null
 service ipsec restart 2>/dev/null
 service xl2tpd restart 2>/dev/null
-wget -O /usr/bin/add-l2tp https://marloxxxssh.xyz/add-l2tp.sh && chmod +x /usr/bin/add-l2tp
-wget -O /usr/bin/del-l2tp https://marloxxxssh.xyz/del-l2tp.sh && chmod +x /usr/bin/del-l2tp
-wget -O /usr/bin/add-pptp https://marloxxxssh.xyz/add-pptp.sh && chmod +x /usr/bin/add-pptp
-wget -O /usr/bin/del-pptp https://marloxxxssh.xyz/del-pptp.sh && chmod +x /usr/bin/del-pptp
-wget -O /usr/bin/cek-pptp https://marloxxxssh.xyz/cek-pptp.sh && chmod +x /usr/bin/cek-pptp
+wget -O /usr/bin/add-l2tp https://raw.githubusercontent.com/bokiroke1/sc1/master/add-l2tp.sh && chmod +x /usr/bin/add-l2tp
+wget -O /usr/bin/del-l2tp https://raw.githubusercontent.com/bokiroke1/sc1/master/del-l2tp.sh && chmod +x /usr/bin/del-l2tp
+wget -O /usr/bin/add-pptp https://raw.githubusercontent.com/bokiroke1/sc1/master/add-pptp.sh && chmod +x /usr/bin/add-pptp
+wget -O /usr/bin/del-pptp https://raw.githubusercontent.com/bokiroke1/sc1/master/del-pptp.sh && chmod +x /usr/bin/del-pptp
+wget -O /usr/bin/cek-pptp https://raw.githubusercontent.com/bokiroke1/sc1/master/cek-pptp.sh && chmod +x /usr/bin/cek-pptp
 touch /var/lib/premium-script/data-user-l2tp
 touch /var/lib/premium-script/data-user-pptp
 rm -f /root/ipsec.sh
